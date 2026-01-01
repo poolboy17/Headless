70fc6766f286e606490c866445434349fad008c0
genaromvasquez@gmail.com <genaromvasquez@gmail.com>
2025-12-31 22:33:01 -0600

    Remove matcher config - run middleware on all routes by default

--- middleware.ts
+++ middleware.ts
@@ -36,16 +36,3 @@ export async function middleware(request: NextRequest) {
   
   return NextResponse.next();
 }
-
-export const config = {
-  matcher: [
-    /*
-     * Match all request paths except:
-     * - _next/static (static files)
-     * - _next/image (image optimization)
-     * - favicon.ico (favicon file)
-     * - public folder files
-     */
-    '/((?!_next/static|_next/image|favicon.ico|favicons|images|.*\\.png$|.*\\.jpg$|.*\\.svg$|.*\\.ico$).*)',
-  ],
-};
